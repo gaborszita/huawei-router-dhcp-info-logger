06650c16f6231413040b353b00697e316d986c32
Gabor Szita <gaborszita88@gmail.com>
2021-11-28 22:47:34 +0100

    Convert to the ip info getter driver

--- ip_info_getter.py
+++ ip_info_getter.py
@@ -3,43 +3,45 @@ from typing import List
 from pyppeteer import launch
 import config
 
-async def main() -> List:
-    ip_info = []
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto("http://192.168.0.1")
-    # login
-    await page.type("#txt_Username", config.ROUTER_USERNAME)
-    await page.type("#txt_Password", config.ROUTER_PASSWORD)
-    await page.keyboard.press("Enter")
-    await page.waitForNavigation()
-    await page.waitForSelector('li[name="subli_dhcpinfo"]')
-    # dhcp info
-    await page.click('li[name="subli_dhcpinfo"]')
-    await page.waitForSelector("#frameContent")
-    frame = page.frames[1]
-    await frame.waitForSelector("#dhcpinfodat")
-    await frame.waitForXPath('//*[contains(@class, "tabal_01")]/td[text() != "--"]')
-    # get data and add it to the ip_info array
-    rows = await frame.JJ(".tabal_01")
-    for row in rows:
-        row = await row.JJeval("td", "(nodes => nodes.map(n => n.innerText))")
-        row_data = []
-        for i in range(len(row)):
-            if i==len(row)-1:
-                data = row[i].replace(u"\xa0", " ")
-                row_data.append(data)
-            else:
-                data = row[i][:-1].replace(u"\xa0", " ")
-                row_data.append(data)
-        ip_info.append(row_data)
-    # logout
-    await page.click("#headerLogoutText")
-    await page.waitForNavigation()
-    # await page.screenshot({'path': 'example.png'})
-    await browser.close()
-    return ip_info
+# This file contains the main driver to get IP info from the router
 
-ip_info = asyncio.get_event_loop().run_until_complete(main())
-for row in ip_info:
-    print(row)
+class IpInfoGetter:
+    async def __get_ip_info_router_comm() -> List:
+        ip_info = []
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto("http://192.168.0.1")
+        # login
+        await page.type("#txt_Username", config.ROUTER_USERNAME)
+        await page.type("#txt_Password", config.ROUTER_PASSWORD)
+        await page.keyboard.press("Enter")
+        await page.waitForNavigation()
+        await page.waitForSelector('li[name="subli_dhcpinfo"]')
+        # dhcp info
+        await page.click('li[name="subli_dhcpinfo"]')
+        await page.waitForSelector("#frameContent")
+        frame = page.frames[1]
+        await frame.waitForSelector("#dhcpinfodat")
+        await frame.waitForXPath('//*[contains(@class, "tabal_01")]/td[text() != "--"]')
+        # get data and add it to the ip_info array
+        rows = await frame.JJ(".tabal_01")
+        for row in rows:
+            row = await row.JJeval("td", "(nodes => nodes.map(n => n.innerText))")
+            row_data = []
+            for i in range(len(row)):
+                if i==len(row)-1:
+                    data = row[i].replace(u"\xa0", " ")
+                    row_data.append(data)
+                else:
+                    data = row[i][:-1].replace(u"\xa0", " ")
+                    row_data.append(data)
+            ip_info.append(row_data)
+        # logout
+        await page.click("#headerLogoutText")
+        await page.waitForNavigation()
+        # await page.screenshot({'path': 'example.png'})
+        await browser.close()
+        return ip_info
+
+    def get_ip_info() -> List:
+        return asyncio.get_event_loop().run_until_complete(IpInfoGetter.__get_ip_info_router_comm())
